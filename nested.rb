2c920cec23054dd72664c1aa3de7aaa089139d12
Laina Karosic <lainakarosic@gmail.com>
2019-09-10 02:38:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,25 +21,24 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-puts programmer_hash[:alan_kay][:known_for]
 
-
-	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-end
+		programmer_hash =
+	 		{
+	        :grace_hopper => {
+	          :known_for => "COBOL",
+	          :languages => ["COBOL", "FORTRAN"]
+	        },
+	        :alan_kay => {
+	          :known_for => "Object Orientation",
+	          :languages => ["Smalltalk", "LISP"]
+	        },
+	        :dennis_ritchie => {
+	          :known_for => "Unix",
+	          :languages => ["C"]
+	        }
+	     }
+			 return programmer_hash[:alan_kay][:known_for]
+	end
 
 def dennis_ritchies_language
 	programmer_hash =
@@ -57,6 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+		 
 end
 
 def adding_matz
